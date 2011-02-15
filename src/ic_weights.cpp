e4f765a526b98e483e5bfb31b4416f494459f79e
Ryan Murray <rymurr@gmail.com>
2011-02-15 17:29:14 +0000

    added weights functions to code base

---
+++ src/ic_weights.cpp
@@ -0,0 +1,6 @@
+
+#include "ic_weights.hpp"
+
+double unit_weight(double inVal){
+    return 1.0;
+}
---
+++ src/ic_weights.hpp
@@ -0,0 +1,35 @@
+#ifndef IC_WEIGHTS_HPP
+#define IC_WEIGHTS_HPP
+
+//#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
+//#include <iterator>
+#include <stdexcept>
+#include <vector>
+//#include <ctime>
+//#include <functional>
+//#include <numeric>
+//#include <typeinfo>
+//#include <gsl/gsl_math.h>
+
+#include <boost/any.hpp>
+//#include <boost/cast.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/casts.hpp>
+#include <boost/shared_ptr.hpp>
+
+
+#include <glog/logging.h>
+
+namespace classical {
+
+double unit_weight(double);
+
+//double atomIonRate(double, double, )
+
+}
+#endif
